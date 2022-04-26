7655ecc8e36b6bee18c1d8700bbfcb9ed0a4dafa
Jaejun Lee <99355321+jaejun-lee06@users.noreply.github.com>
2022-04-26 16:36:39 +0000

    done

--- js/script.js
+++ js/script.js
@@ -5,17 +5,6 @@
 "use strict";
 
 /**
- * Check servie worker.
- */
-if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-Unit5-01/sw.js", {
-    scope: "/ICS2O-Unit5-01/",
-  });
-}
-
-const randomNumber = Math.floor(Math.random() * 6) + 1;
-
-/**
  * This function updates the slider value.
  */
 function updateSliderValue(valueFromSlider) {
@@ -28,16 +17,15 @@ function updateSliderValue(valueFromSlider) {
 function myButtonClicked() {
   const sliderValue = slider.value;
 
-  // process
+  // correct answer
   if (sliderValue == randomNumber) {
     document.getElementById("answer").innerHTML =
       "The answer was, " + randomNumber + "!" + " Great job!";
-    // console.log("Congratulations!")
+
   }
-  //  block of code to be executed if condition1 is true
+  //  incorrect answer
   if (sliderValue != randomNumber) {
     document.getElementById("answer").innerHTML =
       "The answer was, " + randomNumber + "!" + " Good try";
-    // console.log("Try again!")
   }
 }
